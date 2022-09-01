ce6d81dd9569ab9d1de785b5946ac6125121ff1c
Tictim <ksw07149@gmail.com>
2022-09-01 23:17:51 +0900

    ore

--- src/main/java/tictim/hearthstones/contents/block/AquamarineOreBlock.java
+++ src/main/java/tictim/hearthstones/contents/block/AquamarineOreBlock.java
@@ -4,10 +4,12 @@ import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import tictim.hearthstones.contents.ModItems;
 
 import java.util.Random;
 
@@ -16,6 +18,20 @@ public class AquamarineOreBlock extends Block{
 		super(Material.ROCK);
 		this.setHardness(3.0F).setResistance(5.0F);
 		this.setSoundType(SoundType.STONE);
+	}
+
+	@Override public Item getItemDropped(IBlockState state, Random rand, int fortune){
+		return ModItems.AQUAMARINE;
+	}
+
+	@Override public int quantityDropped(Random random){
+		return 2;
+	}
+
+	@Override public int quantityDroppedWithBonus(int fortune, Random random){
+		return fortune>0 ?
+				this.quantityDropped(random)*Math.min(1, random.nextInt(fortune+2)) :
+				this.quantityDropped(random);
 	}
 
 	@Override public int getExpDrop(IBlockState state, IBlockAccess world, BlockPos pos, int fortune){
