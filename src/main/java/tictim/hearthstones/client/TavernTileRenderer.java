92864303a360971be129db8fdd1c86cdaf6830a5
Tictim <ksw07149@gmail.com>
2022-09-05 08:02:48 +0900

    disable tavern nameplate

--- src/main/java/tictim/hearthstones/client/TavernTileRenderer.java
+++ src/main/java/tictim/hearthstones/client/TavernTileRenderer.java
@@ -8,40 +8,9 @@ public class TavernTileRenderer extends TileEntitySpecialRenderer<TavernTile>{
 	@Override
 	public void render(TavernTile te, double x, double y, double z, float partialTicks, int destroyStage, float alpha){
 		if(te==null) return;
-		if(te.hasCustomName()&&
-				this.rendererDispatcher.cameraHitResult!=null&&
-				te.getPos().equals(this.rendererDispatcher.cameraHitResult.getBlockPos()))
-			this.drawTavernNameplate(te, x, y, z);
-
 		GL11.glPushMatrix();
 		GL11.glTranslated(x, y, z);
 		TavernRenderer.renderBlock(te, this.rendererDispatcher.renderEngine, destroyStage, alpha);
 		GL11.glPopMatrix();
-	}
-
-	private void drawTavernNameplate(TavernTile te, double x, double y, double z){
-		switch(this.rendererDispatcher.cameraHitResult.sideHit){
-			case EAST:
-				x += 0.5D;
-				break;
-			case NORTH:
-				z -= 0.5D;
-				break;
-			case SOUTH:
-				z += 0.5D;
-				break;
-			case WEST:
-				x -= 0.5D;
-				break;
-			case UP:
-				y += 0.5D;
-				break;
-			case DOWN:
-				--y;
-		}
-
-		this.setLightmapDisabled(true);
-		this.drawNameplate(te, te.getName(), x, y-0.5D, z, 12);
-		this.setLightmapDisabled(false);
 	}
 }
